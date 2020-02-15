9e47b9f15bc3314e74d288daa6c01588d6415b76
Farid Murzone <faridmurzone@gmail.com>
2020-02-15 09:26:00 -0300

    Cleaned boilerplate crap

--- src/store/sagas/helloSaga.js
+++ src/store/sagas/helloSaga.js
@@ -4,6 +4,9 @@ function* fetchUsers() {
   const json = yield fetch('https://api.org/v1/users')
         .then(response => response.json(), );    
           yield put({ type: "USERS_RECEIVED", json: json.users, })
+        .catch(err => {
+          console.log(err)
+        })
 }
 
 function* actionWatcher() {
